77c6c8b0e6c4d557cc5a90f6e78bc91dc22e0666
Laurence Davies <laurence.davies@ga.gov.au>
2017-02-16 19:33:49 +1100

    Fixed dnbr issue (afaik) in lr.py so that the classifer seems to work.

--- lr.py
+++ lr.py
@@ -10,6 +10,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 import sys
 import math
 
@@ -29,46 +32,40 @@ class FireRegion(object):
 		self.dnbr_dir = '/g/data/r78/lsd547/landsat_dnbr/' # /local/r78
 		self.dnbr_area_fn=self.dnbr_dir+self.dnbr_fn
 		self.dnbr_area=np.loadtxt(self.dnbr_area_fn)
+		self.dnbr_threshold = 0.27
+		self.cw = 40
 	def localise(self,x,y):
 		(nx,ny) = self.dnbr_area.shape
 		#print "DNBR size"
 		#print dnbr_area.shape
-		print (x,y)
-		w = self.dnbr_area_extents[1]-self.dnbr_area_extents[0]
+		#print (x,y)
+		w = self.dnbr_area_extents[0]-self.dnbr_area_extents[1]
 		h = self.dnbr_area_extents[3]-self.dnbr_area_extents[2]
 		tx = nx * (x - self.dnbr_area_extents[1]) / w
-		ty = ny * (y - self.dnbr_area_extents[3]) / h
+		ty = ny * (y - self.dnbr_area_extents[2]) / h
 		return (tx,ty)
 	def getdnbr(self,x,y):
 		(tx,ty) = self.localise(x,y)
-		return np.mean(self.dnbr_area[tx-20:tx+20,ty-20:ty+20])
-		#hs_cols = 9
-		#hs_h8[i,hs_cols + 1] = dval
-		#hs_h8[i,hs_cols + 2] = ndvival
-		#hs_h8[i,hs_cols + 3] = rawndvival
-		#hs_h8[i,hs_cols + 4] = dnbrval
-		#hs_h8[i,hs_cols + 5] = nbrval
-		#hs_h8[i,hs_cols + 6] = rawnbrval
-		#hs_h8[i,hs_cols + 7] = b6val
-		#hs_h8[i,hs_cols + 8] = b7val
-		#hs_h8[i,hs_cols + 9] = b14val
-		#hs_h8[i,hs_cols + 10] = gross_val
-		#hs_h8[i,hs_cols + 11] = thin_val
-		#hs_h8[i,hs_cols + 12] = fog_val
-		#hs_h8[i,hs_cols + 13] = fog2_val
-		#hs_h8[i,hs_cols + 17] = row
-		#hs_h8[i,hs_cols + 16] = col
-		#hs_h8[i,hs_cols + 15] = epochdelta
-		#hs_h8[i,hs_cols + 0] =  in date range of band data
+		#return np.mean(self.dnbr_area[int(tx-40):int(tx+40),int(ty-40):int(ty+40)])
+		return np.nanpercentile(self.dnbr_area[int(ty-self.cw):int(ty+self.cw),int(tx-self.cw):int(tx+self.cw)],90)
+		#try:
+		#	return np.nanmax(self.dnbr_area[int(ty-self.cw):int(ty+self.cw),int(tx-self.cw):int(tx+self.cw)])
+		#except ValueError:
+		#	return None
 	def isfire(self,x,y,d):
 		# use date 2016-01-14 04:12:00 to classify training set.
 		# TODO clear hs between midnight and 4:12am
-		if d >= self.fire_after and self.getdnbr(x,y) >= 0.27:
+		if d >= self.fire_after and self.getdnbr(x,y) >= self.dnbr_threshold:
 			return 1
 		else:
 			return 0
+	def classConfidence(self,x,y,d):
+		if d < self.fire_after:
+			return 1
+		sf = 0.3
+		return np.clip(np.absolute(self.dnbr_threshold - self.getdnbr(x,y)),0,sf) / sf # * 4 / 3 #* 0.5
 	def hasdnbr(self,x,y):
-		if self.dnbr_area_extents[1] <= x <= self.dnbr_area_extents[0] and self.dnbr_area_extents[3] <= y <= self.dnbr_area_extents[2] and np.isfinite(self.getdnbr(x,y)).all():
+		if self.dnbr_area_extents[1] + self.cw <= x <= self.dnbr_area_extents[0] - self.cw and self.dnbr_area_extents[3] +self.cw <= y <= self.dnbr_area_extents[2] - self.cw and np.isfinite(self.getdnbr(x,y)).all():
 			return 1
 		else:
 			return 0
@@ -81,10 +78,37 @@ class FireRegion(object):
 				self.nanrows.append(index)
 			index += 1
 		
+		#hs_cols = 9
+		#hs_h8[i,hs_cols + 1] = dval
+		#hs_h8[i,hs_cols + 2] = ndvival
+		#hs_h8[i,hs_cols + 3] = rawndvival
+		#hs_h8[i,hs_cols + 4] = dnbrval
+		#hs_h8[i,hs_cols + 5] = nbrval
+		#hs_h8[i,hs_cols + 6] = rawnbrval
+		#hs_h8[i,hs_cols + 7] = b4val
+		#hs_h8[i,hs_cols + 8] = b6val
+		#hs_h8[i,hs_cols + 9] = b7val
+		#hs_h8[i,hs_cols + 10] = b14val
+		#hs_h8[i,hs_cols + 11] = b4avgval
+		#hs_h8[i,hs_cols + 12] = b6avgval
+		#hs_h8[i,hs_cols + 13] = b7avgval
+		#hs_h8[i,hs_cols + 14] = b14avgval
+		#hs_h8[i,hs_cols + 15] = gross_val
+		#hs_h8[i,hs_cols + 16] = thin_val
+		#hs_h8[i,hs_cols + 17] = fog_val
+		#hs_h8[i,hs_cols + 18] = fog2_val
+		#hs_h8[i,hs_cols + 19] = daytime flag
+		#hs_h8[i,hs_cols + 20] = epochdelta
+		#hs_h8[i,hs_cols + 21] = row
+		#hs_h8[i,hs_cols + 22] = col
+		#hs_h8[i,hs_cols + 0] =  in date range of band data
 		#X = hs_h8[:,[12,15]]
 		#X = hs_h8[:,[10,11,12,13,14,15,16,17,18,19,20,21,22,24]]
 		#X = hs_h8[:,[12,15,16,17,18,19,20,21,22,24]]
-		self.X = self.hs_h8[:,[8,9,12,15,19,20,21,22,24]]
+		#self.X = self.hs_h8[:,[6,7,8,9,12,15,19,20,21,22,24]]
+		#self.X = self.hs_h8[:,[8,9,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27]]
+		#self.X = self.hs_h8[:,[8,11,14,16,17,18,20,21,22,23,24,25,26]]
+		self.X = self.hs_h8[:,[8,9,11,14,17,21]]
 		(xrows, xcols) = self.X.shape
 		rings = 3
 		ringradius = 2000
@@ -122,7 +146,8 @@ class FireRegion(object):
 		Yrows = np.delete(Yrows,self.nanrows,axis=0)
 		#Y = np.array([isfire(x,y,d) for x,y,d in np.nditer(hs_h8[:,[0,1,2]])])
 		self.Y = np.array([self.isfire(row[0],row[1],row[2]) for row in Yrows])
-		self.W = np.absolute(np.clip(np.array([self.getdnbr(row[0],row[1]) for row in Yrows]),0,1) - 1 + self.Y)
+		#self.W = np.absolute((np.clip(np.array([self.getdnbr(row[0],row[1]) for row in Yrows]),0,1) - self.dnbr_threshold) * (2*self.Y-1) * 0.5 + 0.5)
+		self.W = np.array([self.classConfidence(row[0],row[1],row[2]) for row in Yrows])
 
 regions = [FireRegion(hs_h8_file='WA_20160101_30_H8_ALBERS.csv',
 		dnbr_fn='waroona_landsat_dnbr.txt',
@@ -133,7 +158,33 @@ regions = [FireRegion(hs_h8_file='WA_20160101_30_H8_ALBERS.csv',
 		dnbr_fn='tas_landsat_dnbr.txt',
 		dnbr_area_extents=[1159237.5,1074712.5,-4514812.5,-4598387.5],
 		fire_after= datestr2num('2016-01-14 04:12:00'),
-		nofire_before= datestr2num('2016-01-13 20:12:00'))]
+		nofire_before= datestr2num('2016-01-13 20:12:00')),
+	FireRegion(hs_h8_file='SA_regional_20151201_31_H8_ALBERS.csv',
+		dnbr_fn='sa_regional_subset_landsat_dnbr.txt',
+		dnbr_area_extents=[717437.5,567912.5,-3235887.5,-3380187.5],
+		fire_after= datestr2num('2015-12-02 04:00:00'),
+		nofire_before= datestr2num('2015-12-02 00:00:00'))
+	#FireRegion(hs_h8_file='SA_regional_20151201_31_H8_ALBERS.csv',
+	#	dnbr_fn='sa_regional_landsat_dnbr_1.txt',
+	#	dnbr_area_extents=[762237.5,512212.5,-3128537.5,-3353737.5],
+	#	fire_after= datestr2num('2015-12-02 04:38:00'), # Fixme when was the fire
+	#	nofire_before= datestr2num('2016-12-02 00:38:00')),
+	#FireRegion(hs_h8_file='SA_regional_20151201_31_H8_ALBERS.csv',
+	#	dnbr_fn='sa_regional_landsat_dnbr_2.txt',
+	#	dnbr_area_extents=[993362.5,739612.5,-3140512.5,-3369937.5],
+	#	fire_after= datestr2num('2015-12-12 04:12:00'), # Fixme when was the fire
+	#	nofire_before= datestr2num('2016-12-11 20:12:00')),
+	#FireRegion(hs_h8_file='SA_regional_20151201_31_H8_ALBERS.csv',
+	#	dnbr_fn='sa_regional_landsat_dnbr_3.txt',
+	#	dnbr_area_extents=[749762.5,503687.5,-3330212.5,-3554862.5],
+	#	fire_after= datestr2num('2015-12-02 04:38:00'), # Fixme when was the fire
+	#	nofire_before= datestr2num('2016-12-02 00:38:00')),
+	#FireRegion(hs_h8_file='SA_regional_20151201_31_H8_ALBERS.csv',
+	#	dnbr_fn='sa_regional_landsat_dnbr_4.txt',
+	#	dnbr_area_extents=[977112.5,727312.5,-3341962.5,-3570787.5],
+	#	fire_after= datestr2num('2015-12-12 04:12:00'), # Fixme when was the fire
+	#	nofire_before= datestr2num('2016-12-11 20:12:00'))
+	]
 
 for r in regions:
 	r.preprocess()
@@ -141,79 +192,151 @@ for r in regions:
 #action = 'pca'
 action = 'classify2'
 if action=='classify2':
-	train_rgn = regions[1]
-	test_rgn = regions[0]
+	train_rgn = regions[1] #Tas # Waroona
+	train_rgn2 = regions[0] # SA Regional subset
+	#train_rgn3 = regions[3] # SA Regional
+	#train_rgn4 = regions[4] # SA Regional
+	#train_rgn5 = regions[5] # SA Regional
+	test_rgn = regions[2] #Waroona# Tas
 	# separate into train and test
 	#xrows,xcols = train_rgn.X.shape
-	X = np.concatenate((train_rgn.X,test_rgn.X),axis=0)
-	Y = np.concatenate((train_rgn.Y,test_rgn.Y),axis=0)
-	W = np.concatenate((train_rgn.W,test_rgn.W),axis=0)
-	xrows,xcols = X.shape
-	print "X shape "
-	print (xrows,xcols)
-	train_b = np.random.randint(0,2,size=xrows)
-	train_l = np.where(train_b)
-	test_l = np.where(1-train_b)
-	
-	Xtrain = np.delete(arr=X,obj=train_l,axis=0)
-	Ytrain = np.delete(arr=Y,obj= train_l,axis=0)
-	Wtrain = np.delete(arr=W,obj= train_l,axis=0)
-	#Xtrain = train_rgn.X
-	#Ytrain = train_rgn.Y
-	#Wtrain = train_rgn.W
 
-	Xtest = np.delete(X,test_l,axis=0)
-	Ytest = np.delete(Y,test_l,axis=0)
-	Wtest = np.delete(W,test_l,axis=0)
-	#Xtest = test_rgn.X
-	#Ytest = test_rgn.Y
-	#Wtest = test_rgn.W
+	traintestsel = 'wholearea'
+	#traintestsel = 'randmixed'
+	if traintestsel == 'randmixed':
+		X = np.concatenate((train_rgn.X,test_rgn.X),axis=0)
+		Y = np.concatenate((train_rgn.Y,test_rgn.Y),axis=0)
+		W = np.concatenate((train_rgn.W,test_rgn.W),axis=0)
+		xrows,xcols = X.shape
+		print "X shape "
+		print (xrows,xcols)
+		train_b = np.random.randint(0,2,size=xrows)
+		train_l = np.where(train_b)
+		test_l = np.where(1-train_b)
+		Xtrain = np.delete(arr=X,obj=train_l,axis=0)
+		Ytrain = np.delete(arr=Y,obj= train_l,axis=0)
+		Wtrain = np.delete(arr=W,obj= train_l,axis=0)
+		Xtest = np.delete(X,test_l,axis=0)
+		Ytest = np.delete(Y,test_l,axis=0)
+		Wtest = np.delete(W,test_l,axis=0)
+	else:
+		#Xtrain = train_rgn.X
+		#Ytrain = train_rgn.Y
+		#Wtrain = train_rgn.W
+		#Xtrain = np.concatenate((train_rgn.X,train_rgn2.X,train_rgn3.X,train_rgn4.X,train_rgn5.X),axis=0)
+		#Ytrain = np.concatenate((train_rgn.Y,train_rgn2.Y,train_rgn3.Y,train_rgn4.Y,train_rgn5.Y),axis=0)
+		#Wtrain = np.concatenate((train_rgn.W,train_rgn2.W,train_rgn3.W,train_rgn4.W,train_rgn5.W),axis=0)
+		Xtrain = np.concatenate((train_rgn.X,train_rgn2.X),axis=0)
+		Ytrain = np.concatenate((train_rgn.Y,train_rgn2.Y),axis=0)
+		Wtrain = np.concatenate((train_rgn.W,train_rgn2.W),axis=0)
+		
+		Xtest = test_rgn.X
+		Ytest = test_rgn.Y
+		Wtest = test_rgn.W
 
 	print "Train size " + str(len(Xtrain)) + " " + str(len(Ytrain))
 
-	#logreg = linear_model.LogisticRegression(C=1e5)
-	#logreg = DecisionTreeClassifier(random_state=0)
-	#logreg = SVC(kernel='linear',degree=3,verbose=True,probability=True)
-	#logreg = SVC(verbose=True,probability=True)
-	logreg = RandomForestClassifier(verbose=True)
-	#logreg = MLPClassifier(solver='lbfgs',verbose=True)
-	logreg.fit(Xtrain, Ytrain, sample_weight=Wtrain)
-	#logreg.fit(Xtrain, Ytrain)
-	Ypred = logreg.predict_proba(Xtest)[:,1]
+	# check for nans
+	(rr,cc) = Xtrain.shape # rows cols
+	for c_ in xrange(cc):
+		if not np.isfinite(Xtrain[:,c_]).any():
+			print "Train Column " + c_ + " contains a non-finite value"
 
-	Ypred_0 = np.delete(Ypred,np.where(Ytest),axis=0)
-	Ypred_1 = np.delete(Ypred,np.where(1-Ytest),axis=0)
+	# check for nans
+	(rr,cc) = Xtest.shape # rows cols
+	for c_ in xrange(cc):
+		if not np.isfinite(Xtest[:,c_]).any():
+			print "Test Column " + c_ + " contains a non-finite value"
+
+	#clsfr = linear_model.LogisticRegression(C=1e5)
+	#clsfr = BaggingClassifier(linear_model.LogisticRegression(C=1e2),n_estimators=100,bootstrap_features=False)
+	#clsfr = BaggingClassifier(SVC(verbose=True,probability=True),n_estimators=100,bootstrap_features=False)
+	#clsfr = AdaBoostClassifier(SVC(verbose=True,probability=True),n_estimators=50)
+	clsfr = GradientBoostingClassifier(n_estimators=10000, learning_rate=0.3, max_depth=3, random_state=0)
+	#clsfr = DecisionTreeClassifier(random_state=0)
+	#clsfr = SVC(kernel='linear',degree=3,verbose=True,probability=True)
+	#clsfr = SVC(verbose=True,probability=True)
+	#clsfr = RandomForestClassifier(verbose=True)
+	#clsfr = MLPClassifier(solver='lbfgs',verbose=True)
+
+	clsfr.fit(Xtrain, Ytrain, sample_weight=Wtrain)
+	print "Classes: " + str(clsfr.classes_)
+
+	print "Score: " + str(clsfr.score(Xtest,Ytest))
+
+	#clsfr.fit(Xtrain, Ytrain)
+	Ypred_class_prob = clsfr.predict_proba(Xtest)
+	Ypred_classification = clsfr.predict(Xtest)
+	#print Ypred_class_prob
+	Ypred_0 = Ypred_class_prob[:,0]
+	Ypred_1 = Ypred_class_prob[:,1]
+
+
+	print 'Number of hotspots classified as fire (Positive class): ' + str(np.sum(Ytest)) + " vs predicted " + str(np.sum(Ypred_classification)) + " or ll weighted " + str(np.sum(Ypred_1))
+	print 'Number of hotspots classified as non-fire (Negative class): ' + str(np.sum(1-Ytest)) + " vs predicted " + str(np.sum(1-Ypred_classification)) + " or ll weighted " + str(np.sum(Ypred_0))
+
+	Ypred_0_true = np.delete(Ypred_0,np.where(Ytest),axis=0)
+	Ypred_1_true = np.delete(Ypred_1,np.where(1-Ytest),axis=0)
+	Ypred_0_false = np.delete(Ypred_0,np.where(1-Ytest),axis=0)
+	Ypred_1_false = np.delete(Ypred_1,np.where(Ytest),axis=0)
+
+	print "Predicted lh false negative scores"
+	print np.sum(Ypred_0_false)
+	print "Predicted lh true negative scores"
+	print np.sum(Ypred_0_true)
+	print "Predicted lh true positive scores"
+	print np.sum(Ypred_1_true)
+	print "Predicted lh false positive scores"
+	print np.sum(Ypred_1_false)
 
 	print "Predicted false negative scores"
-	print np.sum(Ypred_0)
+	print np.sum(np.logical_and(1-Ypred_classification,Ytest))
 	print "Predicted true negative scores"
-	print len(Ypred_0) - np.sum(Ypred_0)
+	print np.sum(np.logical_and(1-Ypred_classification,1-Ytest))
 	print "Predicted true positive scores"
-	print np.sum(Ypred_1)
+	print np.sum(np.logical_and(Ypred_classification,Ytest))
 	print "Predicted false positive scores"
-	print len(Ypred_1) - np.sum(Ypred_1)
+	print np.sum(np.logical_and(Ypred_classification,1-Ytest))
 
-	print "Test size " + str(len(Xtest)) + " " + str(len(Ytest)) + " " + str(len(Ypred))
-	#for yp in Ypred:
-	#	print yp
+	print "Test size " + str(len(Xtest)) + " " + str(len(Ytest)) + " " + str(len(Ypred_0))
 
+	#f, ((ax1,ax2),(ax3,ax4), (ax5,ax6)) = plt.subplots(3,2)
 	f, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2)
 
-	ax3.scatter(Ytest,Ypred,c=np.square(Ytest-Ypred),label='Test')
 	#ax3.scatter(np.arange(len(Ytest)),Ytest,c='navy',label='Test')
 	#ax3.scatter(np.arange(len(Ypred)),Ypred,c='darkorange',label='Predicted')
-	ax3.set_title('Classification')
-	ax3.legend(loc='lower right')
+	#ax3.scatter(Ytest,Ypred,c=np.square(Ytest-Ypred),label='Test')
+	#ax3.set_title('Classification')
+	#ax3.legend(loc='lower right')
 
-	fpr,tpr,thresholds = metrics.roc_curve(Ytest,Ypred,pos_label=1,drop_intermediate=False)
+	ax3.hist(Ypred_1_true,10)
+	ax3.set_title('True Positive Rate')
+	ax4.hist(Ypred_0_true,10)
+	ax4.set_title('True Negative Rate')
+	#ax5.hist(Ypred_1,10)
+	#ax5.set_title('Probabilities of Positive Classification')
+	#ax6.hist(Ypred_0,10)
+	#ax6.set_title('Probabilities of Negative Classification')
+
+	fpr,tpr,thresholds = metrics.roc_curve(Ytest,Ypred_1,pos_label=1,drop_intermediate=False)
 	roc_auc = metrics.auc(fpr,tpr)
 
-	print "FPR"
-	print fpr
-	print "TPR"
-	print tpr
-	print "thresholds"
-	print thresholds
+	#print "FPR"
+	#print fpr
+	#print "TPR"
+	#print tpr
+	#print "thresholds"
+	#print thresholds
+
+	fprc,tprc,thresholdsc = metrics.roc_curve(Ytest,Ypred_classification,pos_label=1,drop_intermediate=False)
+	roc_aucc = metrics.auc(fprc,tprc)
+
+	#print "FPR"
+	#print fprc
+	#print "TPR"
+	#print tprc
+	#print "thresholds"
+	#print thresholdsc
 
 	lw = 2
 
@@ -227,8 +350,39 @@ if action=='classify2':
 	ax1.set_title('Receiver operating characteristic test data')
 	ax1.legend(loc="lower right")
 
-	Ypred2 = logreg.predict_proba(Xtrain)[:,1]
-	fpr2,tpr2,thresholds2 = metrics.roc_curve(Ytrain,Ypred2,pos_label=1)
+	Ypred2 = clsfr.predict_proba(Xtrain)
+	Ypred2_0 = Ypred2[:,0]
+	Ypred2_1 = Ypred2[:,1]
+	Ypred2_classification = clsfr.predict(Xtrain)
+	if True:
+		Ypred2_0_true = np.delete(Ypred2_0,np.where(Ytrain),axis=0)
+		Ypred2_1_true = np.delete(Ypred2_1,np.where(1-Ytrain),axis=0)
+		Ypred2_0_false = np.delete(Ypred2_0,np.where(1-Ytrain),axis=0)
+		Ypred2_1_false = np.delete(Ypred2_1,np.where(Ytrain),axis=0)
+	
+		print 'Number of train hotspots classified as fire (Positive class): ' + str(np.sum(Ytrain)) + " vs predicted " + str(np.sum(Ypred2_classification)) + " or ll weighted " + str(np.sum(Ypred2_1))
+		print 'Number of train hotspots classified as non-fire (Negative class): ' + str(np.sum(1-Ytrain)) + " vs predicted " + str(np.sum(1-Ypred2_classification)) + " or ll weighted " + str(np.sum(Ypred2_0))
+		print "Predicted Train lh false negative scores"
+		print np.sum(Ypred2_0_false)
+		print "Predicted Train lh true negative scores"
+		print np.sum(Ypred2_0_true)
+		print "Predicted Train lh true positive scores"
+		print np.sum(Ypred2_1_true)
+		print "Predicted Train lh false positive scores"
+		print np.sum(Ypred2_1_false)
+	
+		print "Predicted Train false negative scores"
+		print np.sum(np.logical_and(1-Ypred2_classification,Ytrain))
+		print "Predicted Train true negative scores"
+		print np.sum(np.logical_and(1-Ypred2_classification,1-Ytrain))
+		print "Predicted Train true positive scores"
+		print np.sum(np.logical_and(Ypred2_classification,Ytrain))
+		print "Predicted Train false positive scores"
+		print np.sum(np.logical_and(Ypred2_classification,1-Ytrain))
+	
+		print "Train size " + str(len(Xtrain)) + " " + str(len(Ytrain)) + " " + str(len(Ypred2_0))
+
+	fpr2,tpr2,thresholds2 = metrics.roc_curve(Ytrain,Ypred2_1,pos_label=1)
 	roc_auc2 = metrics.auc(fpr2,tpr2)
 	
 	ax2.plot(fpr2, tpr2, color='darkorange',
@@ -244,10 +398,10 @@ if action=='classify2':
 	plt.show()
 if action=='classify':
 	h = .02  # step size in the mesh
-	logreg = linear_model.LogisticRegression(C=1e5)
+	clsfr = linear_model.LogisticRegression(C=1e5)
 	
 	# we create an instance of Neighbours Classifier and fit the data.
-	logreg.fit(X, Y)
+	clsfr.fit(X, Y)
 	
 	# Plot the decision boundary. For that, we will assign a color to each
 	# point in the mesh [x_min, x_max]x[y_min, y_max].
@@ -256,7 +410,7 @@ if action=='classify':
 	x_min, x_max = X[:, xaxis].min() - .5, X[:, xaxis].max() + .5
 	y_min, y_max = X[:, yaxis].min() - .5, X[:, yaxis].max() + .5
 	xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-	Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+	Z = clsfr.predict(np.c_[xx.ravel(), yy.ravel()])
 	
 	# Put the result into a color plot
 	Z = Z.reshape(xx.shape)
